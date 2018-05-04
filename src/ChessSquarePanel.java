32f89453f721e1e6b5d9eddf065991b43463c4ac
scannella <scannella@bishopireton.org>
2018-05-04 09:51:10 -0400

    Working on Pseudocode 2

--- src/ChessSquarePanel.java
+++ src/ChessSquarePanel.java
@@ -1,11 +1,6 @@
 import java.awt.Color;
 import java.awt.Font;
 import java.awt.Graphics;
-import java.awt.GridLayout;
-
-import javax.swing.BoxLayout;
-import javax.swing.JButton;
-import javax.swing.JFrame;
 import javax.swing.JPanel;
 
 public class ChessSquarePanel extends JPanel{
@@ -15,11 +10,6 @@ private final static String QUEEN = "Q";
 	
 	private boolean isQueen;
 	private Color backColor;
-	
-	ChessSquarePanel() {
-		backColor = Color.BLUE;
-		isQueen = false;
-	}
 
 	ChessSquarePanel(Color c, boolean b) {
 		backColor = c;
@@ -27,24 +17,17 @@ private final static String QUEEN = "Q";
 	}
 
 	public void paintComponent(Graphics g) {
-		
 		super.paintComponent(g);
 
 		g.setFont(new Font("TimesRoman", Font.BOLD, FONTSIZE));
 		this.setBackground(backColor);
 		g.setColor(Color.BLACK);
-
-        int x = (this.getWidth() / 2) - FONTSIZE/4; 
-        int y = (this.getHeight() / 2) + FONTSIZE/4;
-        if(isQueen) {
+		
+		if(isQueen) {
+			int x = (this.getWidth() / 2) - FONTSIZE/4; 
+			int y = (this.getHeight() / 2) + FONTSIZE/4;
         	g.drawString(QUEEN, x, y);
 		}
 	}
   
-   public void setBackColor(Color c) {
-	   backColor = c;
-	   repaint();   
-   }
-
-
 }
--- src/EightQueens.java
+++ src/EightQueens.java
@@ -6,13 +6,15 @@ import javax.swing.JFrame;
 public class EightQueens {
 	
 public final static int SIZE = 8;
-	
-	
+//boolean[][] works;
 	
 	public static void main(String[] args) {
 		JFrame window = new JFrame("myWindow");
 		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		window.setLayout(new GridLayout(SIZE,SIZE));
+		window.setSize(400, 400);
+		
+		
 		
 		boolean[][] works = {{false, false, false, true, false, false, false, false},
 							 {false, false, false, false, false, false, true, false},
@@ -42,11 +44,6 @@ public final static int SIZE = 8;
 			}
 			
 		}
-		
-		
-		
-		window.pack();
-		window.setSize(1000, 1000);
 		
 		
 		window.setVisible(true);
