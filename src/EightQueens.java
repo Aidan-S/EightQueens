d9f09c8936307f379ad5753f26852a473d460f45
Aidan <Aidan@Aidan-PC>
2018-05-06 12:38:58 -0400

    trying a thing for recursive, spent a while on it

--- src/EightQueens.java
+++ src/EightQueens.java
@@ -1,13 +1,54 @@
 import java.awt.Color;
 import java.awt.GridLayout;
+import java.util.ArrayList;
 
 import javax.swing.JFrame;
 
 public class EightQueens {
 	
 public final static int SIZE = 8;
-//boolean[][] works;
 	
+private ArrayList<Integer> queens; 
+	
+	public boolean addQueens(ArrayList<Integer> array) {
+		if(array.size() == 8) {
+			queens = array;
+			return true;
+		}
+		for(int i = 0; i < array.size(); i++) {
+			int x = array.get(i);
+			int y = i;
+			
+			x = newX(array, x, y);
+			
+		}
+		
+		
+	}
+
+	public int newX(ArrayList<Integer> array, int newx, int newy) {
+		if(array.size() == 0) {
+			return (int)(Math.random() * 8);
+		}
+		
+		for(int k = 0; k < SIZE; k++) {
+			for(int i = 0; i < array.size(); i++) {
+				int x = array.get(i);
+				int y = i;
+				
+				if(k != x) {
+					if() {
+					
+					}
+				}
+				
+				
+			}
+		}
+		return -1;
+	}
+
+
 	public static void main(String[] args) {
 		JFrame window = new JFrame("myWindow");
 		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
